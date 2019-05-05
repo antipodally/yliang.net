6bba97db65c1ddcaede00ea358bf6f8facbba73d
antipodally <antipodally@gmail.com>
2019-05-04 18:30:15 -0700

    new

---
+++ precache-manifest.95bb58fda3f403ae6217d8c98ce9e6d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e046d0a63f60b61633",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/css/main.70ea37bf.chunk.css"
+  },
+  {
+    "revision": "90e046d0a63f60b61633",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/js/main.3b0e320a.chunk.js"
+  },
+  {
+    "revision": "9b1cbfc12f479c181c87",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/js/runtime~main.28b11912.js"
+  },
+  {
+    "revision": "a85de1cbe9b9e09d0a5c",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/css/2.792e5bf0.chunk.css"
+  },
+  {
+    "revision": "a85de1cbe9b9e09d0a5c",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/js/2.bdfe7961.chunk.js"
+  },
+  {
+    "revision": "8fba7efd164c4c76336eaeb106c9a991",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/media/logobw.8fba7efd.png"
+  },
+  {
+    "revision": "b02e95f6a6da583cb0d4b08435a8263c",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/static/media/logoer.b02e95f6.png"
+  },
+  {
+    "revision": "aee2a7034b9fec1ca27face2dac9250e",
+    "url": "https://zion.nyc3.digitaloceanspaces.com/yliang.net/index.html"
+  }
+];

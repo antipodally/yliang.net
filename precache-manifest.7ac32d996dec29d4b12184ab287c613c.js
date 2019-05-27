d5e83d23ed4485f7a62bafeeb4d0008b96d477f5
antipodally <antipodally@gmail.com>
2019-05-26 21:14:59 -0700

    homepage

---
+++ precache-manifest.7ac32d996dec29d4b12184ab287c613c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55e251c4c951146b1e3d",
+    "url": "/static/css/main.97362267.chunk.css"
+  },
+  {
+    "revision": "55e251c4c951146b1e3d",
+    "url": "/static/js/main.620e5430.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0cb26fefc110bd94b6a0",
+    "url": "/static/css/2.12b3f481.chunk.css"
+  },
+  {
+    "revision": "0cb26fefc110bd94b6a0",
+    "url": "/static/js/2.5865b09e.chunk.js"
+  },
+  {
+    "revision": "8fba7efd164c4c76336eaeb106c9a991",
+    "url": "/static/media/logobw.8fba7efd.png"
+  },
+  {
+    "revision": "b02e95f6a6da583cb0d4b08435a8263c",
+    "url": "/static/media/logoer.b02e95f6.png"
+  },
+  {
+    "revision": "c3a5f92c9197b7282b108dd4fc5b9915",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5289f04ad2f36a2d1f5154e3b2958a9c.js"
+  "/precache-manifest.7ac32d996dec29d4b12184ab287c613c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
